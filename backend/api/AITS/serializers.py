58d11726a101deecb915d7216287e3a65a7aad10
user.email <eageno075@gmail.com>
2025-03-13 19:12:21 +0300

    added admin

--- backend/api/AITS/serializers.py
+++ backend/api/AITS/serializers.py
@@ -1,11 +1,19 @@
-from .models import CustomUser, Department, CourseUnit, Program, Issue
 from rest_framework import serializers
-
+from .models import CustomUser, Department, CourseUnit, Program, Issue  
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'Role', 'department', 'program', 'year_of_study', 'password']
+        extra_kwags = {'password': {'write_only': True, 'required': True}} #specifies additional keyword arguments for the password field.
+       #This is important for security reasons, so that passwords are not exposed in API responses
+        # The write_only flag is set to True so that the password is not included in the serialized output.
+        # The required flag is set to True so that the password field is required when data is deserialized.
+
+    def create (self,validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+    
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,6 +34,4 @@ class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
         fields = '__all__'
-
-
-                  
+        
